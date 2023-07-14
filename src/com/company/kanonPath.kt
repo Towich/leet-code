a9b4ab24341257efa3890b6f8bc2bd96a9f49c35
Towich <towichf@gmail.com>
2023-07-14 12:43:28 +0300

    #2 completed: Canonical Path

--- src/com/company/kanonPath.kt
+++ src/com/company/kanonPath.kt
@@ -16,7 +16,6 @@ fun goUpDir(inString: String): String {
 
     return resultStr.dropLast(1)
 }
-
 fun getPath(inStringNotPrepared: String): String {
     var currFolder = ""
     var currDirectory = ""
@@ -47,13 +46,102 @@ fun getPath(inStringNotPrepared: String): String {
         currDirectory
 }
 
+/*
+        /home/root/yandex/..    --->    /home/root
+
+        read after '/' until '/' or last symbol
+
+        currDir = "/home/root/yandex"
+        currFolder = ".."
+
+        currFolder == "." ?
+            yes: clear currFolder and continue
+        currFolder == ".." ?
+            yes: we have to remove last directory in currDir with separator '/', clear currFolder
+        else
+            add '/' and currFolder to end of currDir
+
+ */
+
+fun getDir(inString: String): String {
+    var currFolder = ""
+    var currDir = ""
+    var strPrepared = ""
+
+    var l = 0
+    while(l < inString.length){
+        if(inString[l] == '/'){
+            while(l < inString.length && inString[l] == '/'){
+                l++
+            }
+            strPrepared += '/'
+        }
+        else{
+            strPrepared += inString[l]
+            l++
+        }
+    }
+//    strPrepared = inString.replace("//", "/")
+
+    if(strPrepared.last() == '/')
+        strPrepared = strPrepared.dropLast(1)
+
+    var i = 0
+    while(i < strPrepared.length){
+        val currSym = strPrepared[i]
+
+        if(i == strPrepared.length-1){
+            currFolder += currSym
+        }
+
+        if(currSym == '/' || i == strPrepared.length-1){
+
+            if(currFolder == "."){
+
+            }
+            else if(currFolder == ".."){
+
+                var symbolsToRemove = 0
+                for(k in currDir.indices){
+                    if(currDir[currDir.length-1-k] != '/') {
+                        symbolsToRemove++
+                    }
+                    else{
+                        break
+                    }
+                }
+
+                currDir = currDir.dropLast(symbolsToRemove+1)
+            }
+            else{
+                if(i != 0)
+                    currDir += "/$currFolder"
+            }
+
+            currFolder = ""
+        }
+        else{
+            currFolder += currSym
+        }
+        i++
+    }
+
+    return if(currDir == "")
+        "/"
+    else
+        currDir
+
+}
+
 fun main(){
 
 //    val input = readLine()
-//    println(getPath(input!!))
-    assertEquals("/home/foo/my/year", goUpDir("/home/foo/my/year/ggrewrwefdsfsdg/.."))
-
-    assertEquals("/home", getPath("/home/"))
-    assertEquals("/", getPath("/../"))
-    assertEquals("/home/foo", getPath("/home//foo/"))
+//    println(getDir(input!!))
+    assertEquals("/home/foo", getDir("/home/foo/fsdg/.."))
+    assertEquals("/home", getDir("/home/"))
+    assertEquals("/", getDir("/../"))
+    assertEquals("/home/foo", getDir("/home//foo/"))
+    assertEquals("/home", getDir("/home/foo/gdfgdr/../../"))
+    assertEquals("/home", getDir("/home/foo/gdfgdr/../.././"))
+    assertEquals("/", getDir("//////"))
 }
